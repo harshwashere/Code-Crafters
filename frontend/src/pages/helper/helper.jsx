28fd482919b1c8a2b528bf1bbad9522455c86474
Harsh Chauhan <hc118130@gmail.com>
2024-10-06 17:19:10 +0530

    Updated Code

--- frontend/src/pages/helper/helper.jsx
+++ frontend/src/pages/helper/helper.jsx
@@ -1,2 +1,2 @@
-export const URL = "https://code-crafters-j58r-5u8suvtja-harsh-chauhans-projects-24dc09c6.vercel.app/"
-// https://code-crafters-j58r-5u8suvtja-harsh-chauhans-projects-24dc09c6.vercel.app/
+export const URL = "https://code-crafters-j58r-ofhxxl9n3-harsh-chauhans-projects-24dc09c6.vercel.app/"
+// https://code-crafters-j58r-ofhxxl9n3-harsh-chauhans-projects-24dc09c6.vercel.app/
