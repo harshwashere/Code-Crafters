c17d50512db5f526fc2544d6fcc69c2319961b91
Harsh Chauhan <hc118130@gmail.com>
2024-10-08 01:04:22 +0530

    Update helper.jsx

--- frontend/src/pages/helper/helper.jsx
+++ frontend/src/pages/helper/helper.jsx
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:7000"
-// https://code-crafters-j58r-ofhxxl9n3-harsh-chauhans-projects-24dc09c6.vercel.app/
+export const URL = "https://code-crafters-j58r.vercel.app/"
+// https://code-crafters-j58r.vercel.app/
